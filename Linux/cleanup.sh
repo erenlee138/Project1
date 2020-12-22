5fd93659e595d4d725bd3c7d12bca7475224e8d4
Eren Lee <erenlee@Erens-MacBook-Pro.local>
2020-12-21 19:06:44 -0700

    Second Commit

---
+++ Linux/cleanup.sh
@@ -0,0 +1,14 @@
+#!/bin/bang
+
+rm -rf /tmp/*
+rm -rf /var/tmp*
+
+apt clean -y
+
+rm -rf /home/sysadmin/.cache/thumbnails
+rm -rf /home/instructor/.cache/thumbnails
+rm -rf /home/student/.cache/thumbnails
+rm -rf /root/.cache/thumbnails
+
+
+
---
+++ Linux/update.sh
@@ -0,0 +1,11 @@
+#!/bin/bang
+
+apt update -y
+
+apt upgrade -y
+
+apt full-upgrade -y
+
+apt autoremove --purge -y
+
+apt update -y && aptupgrade -y && apy full-upgrade -y && get autoremove --purge -y
